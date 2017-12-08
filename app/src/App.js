4b0296eeaf618d8c70c947b37ef4d3cad4a29985
Tapio Ikkala <tapio.ikkala@relex.fi>
2017-12-08 18:26:03 +0200

    Leaflet added

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import 'leaflet'
 
 class App extends Component {
   render() {
+
+    let map = L.map('map', {
+      center: [60.184, 24.956],
+      zoom: 14
+    });
+
     return (
-    <p>Hello world!</p>
+    <div>{map}</div>
     );
   }
 }
